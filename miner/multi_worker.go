ca569dbabffd6cdc1a17412aa05acfd4c0ac44fb
Edgar Aroutiounian <edgar.factorial@gmail.com>
2021-04-14 08:03:55 -0400

    [flashbots] Add fullblock worker

--- miner/multi_worker.go
+++ miner/multi_worker.go
@@ -20,70 +20,89 @@ var (
 )
 
 type multiWorker struct {
-	regularWorker   *worker
-	flashbotsWorker *worker
+	regularWorker            *worker
+	flashbotsWorker          *worker
+	flashbotsFullBlockWorker *worker
 }
 
 func (w *multiWorker) stop() {
 	w.regularWorker.stop()
 	w.flashbotsWorker.stop()
+	w.flashbotsFullBlockWorker.stop()
 }
 
 func (w *multiWorker) start() {
 	w.regularWorker.start()
 	w.flashbotsWorker.start()
+	w.flashbotsFullBlockWorker.start()
 }
 
 func (w *multiWorker) close() {
 	w.regularWorker.close()
 	w.flashbotsWorker.close()
+	w.flashbotsFullBlockWorker.close()
 }
 
 func (w *multiWorker) isRunning() bool {
-	return w.regularWorker.isRunning() || w.flashbotsWorker.isRunning()
+	return w.regularWorker.isRunning() ||
+		w.flashbotsWorker.isRunning() ||
+		w.flashbotsFullBlockWorker.isRunning()
 }
 
 func (w *multiWorker) setExtra(extra []byte) {
 	w.regularWorker.setExtra(extra)
 	w.flashbotsWorker.setExtra(extra)
+	w.flashbotsFullBlockWorker.setExtra(extra)
 }
 
 func (w *multiWorker) setRecommitInterval(interval time.Duration) {
 	w.regularWorker.setRecommitInterval(interval)
 	w.flashbotsWorker.setRecommitInterval(interval)
+	w.flashbotsFullBlockWorker.setRecommitInterval(interval)
 }
 
 func (w *multiWorker) setEtherbase(addr common.Address) {
 	w.regularWorker.setEtherbase(addr)
 	w.flashbotsWorker.setEtherbase(addr)
+	w.flashbotsFullBlockWorker.setEtherbase(addr)
 }
 
 func (w *multiWorker) enablePreseal() {
 	w.regularWorker.enablePreseal()
 	w.flashbotsWorker.enablePreseal()
+	w.flashbotsFullBlockWorker.enablePreseal()
 }
 
 func (w *multiWorker) disablePreseal() {
 	w.regularWorker.disablePreseal()
 	w.flashbotsWorker.disablePreseal()
+	w.flashbotsFullBlockWorker.disablePreseal()
 }
 
 func newMultiWorker(config *Config, chainConfig *params.ChainConfig, engine consensus.Engine, eth Backend, mux *event.TypeMux, isLocalBlock func(*types.Block) bool, init bool) *multiWorker {
 	queue := make(chan *task)
 
 	return &multiWorker{
-		regularWorker: newWorker(config, chainConfig, engine, eth, mux, isLocalBlock, init, &flashbotsData{
-			isFlashbots: false,
-			queue:       queue,
-		}),
-		flashbotsWorker: newWorker(config, chainConfig, engine, eth, mux, isLocalBlock, init, &flashbotsData{
-			isFlashbots: true,
-			queue:       queue,
-		}),
+		regularWorker: newWorker(
+			config, chainConfig, engine, eth, mux, isLocalBlock, init, &flashbotsData{
+				queue: queue,
+			}),
+		flashbotsWorker: newWorker(
+			config, chainConfig, engine, eth, mux, isLocalBlock, init, &flashbotsData{
+				isFlashbots: true,
+				queue:       queue,
+			}),
+		flashbotsFullBlockWorker: newWorker(
+			config, chainConfig, engine, eth, mux, isLocalBlock, init, &flashbotsData{
+				isFlashbots:      true,
+				acceptFullBlocks: true,
+				queue:            queue,
+			}),
 	}
 }
 
 type flashbotsData struct {
-	isFlashbots bool
-	queue       chan *task
+	isFlashbots      bool
+	acceptFullBlocks bool
+	queue            chan *task
 }
